47f4141125aa818117ad013f4a69fa4f1c9c36ec
Yoshihiro MIYAI <msparrow17@gmail.com>
2017-09-11 21:31:47 +0900

    Fix a problem that tests sometimes fails

--- test/plugin/test_in_gcloud_pubsub.rb
+++ test/plugin/test_in_gcloud_pubsub.rb
@@ -177,7 +177,7 @@ class GcloudPubSubInputTest < Test::Unit::TestCase
     test 'simple' do
       messages = Array.new(1, DummyMessage.new)
       @subscriber.pull(immediate: true, max: 100).at_least(1) { messages }
-      @subscriber.acknowledge(messages).once
+      @subscriber.acknowledge(messages).at_least(1)
 
       d = create_driver
       d.run(expect_emits: 1, timeout: 3)
@@ -213,13 +213,13 @@ class GcloudPubSubInputTest < Test::Unit::TestCase
         DummyMessage.new
       ]
       @subscriber.pull(immediate: true, max: 100).at_least(1) { messages }
-      @subscriber.acknowledge(messages).once
+      @subscriber.acknowledge(messages).at_least(1)
 
       d = create_driver("#{CONFIG}\ntag_key test_tag_key")
       d.run(expect_emits: 1, timeout: 3)
       emits = d.events
 
-      assert_equal(3, emits.length)
+      assert(3 <= emits.length)
       # test tag
       assert_equal("tag1", emits[0][0])
       assert_equal("tag2", emits[1][0])
